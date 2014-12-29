666c6914192a4853a345d0843ee4e56ad052ddf2
Raul Sierro <raul.sierro@gmail.com>
2014-12-29 01:18:13 +0100

    Control Seleccion terminado

--- ControlSeleccion/app/src/main/java/com/controlseleccion/raul/controlseleccion/MainActivity.java
+++ ControlSeleccion/app/src/main/java/com/controlseleccion/raul/controlseleccion/MainActivity.java
@@ -4,14 +4,75 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //private String[] colores;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RadioGroup rg = (RadioGroup) findViewById(R.id.radioGrup);
+
+        rg.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId){
+                    case R.id.radioButton:
+                        Toast.makeText(getApplicationContext(),"Opcion 1",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioButton2:
+                        Toast.makeText(getApplicationContext(),"Opcion 2",Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.radioButton3:
+                        Toast.makeText(getApplicationContext(),"Opcion 3",Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+
+        /*Spinner sp = (Spinner) findViewById(R.id.spinner);
+
+        ArrayAdapter<CharSequence>adapter;
+        adapter = ArrayAdapter.createFromResource(this,R.array.colores,android.R.layout.simple_spinner_item);
+        sp.setAdapter(adapter);
+
+        sp.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"Has seleccionado el color: "+ R.array.colores,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+         ListView y GridView exactamaente igual
+
+        colores = new String[]{"Rojo","Verde","Azul","Amarillo","Naranja"};
+
+        ArrayAdapter<String>adapter;
+        adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,colores);
+
+        ListView lv = (ListView) findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"Has seleccionado el color: "+ colores[position],Toast.LENGTH_SHORT).show();
+            }
+        });*/
     }
 
 
